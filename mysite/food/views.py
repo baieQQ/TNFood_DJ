fdb510207898f201d891505854f0af464eced7e6
baieQQ <a076907753@gmail.com>
2024-01-03 16:02:43 +0800

    add data

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello food!")
-    return render(request, 'food/index.html',{'place_nums': range(6)})
+    return render(request, 'food/index.html', {'store_list': Place.objects.all(), })
